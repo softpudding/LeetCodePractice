24925860f79518031516a22de07a4d588ef916f0
yy <37563992+llIllIllIlllIll@users.noreply.github.com>
2018-12-27 20:09:35 +0800

    ImplementTrie
    前缀树

---
+++ ImplementTrie.cpp
@@ -0,0 +1,68 @@
+class Trie {
+private:
+    class Node{
+        public:
+        bool isWord;
+        Node * children[26];
+        Node(){
+            isWord=false;
+            for(int i=0;i<26;i++)
+                children[i]=nullptr;
+        };
+    };
+    
+    Node * root;
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new Node();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Node * cur_node= root;
+        int i=0;
+        while(i<word.size()){
+            if(cur_node->children[word[i]-'a']==nullptr)
+            {
+                cur_node->children[word[i]-'a']=new Node();
+            }
+            cur_node=cur_node->children[word[i]-'a'];
+            i++;
+        }
+        cur_node->isWord=true;
+        
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Node * cur_node=root;
+        int i=0;
+        while(i<word.size()){
+            if(cur_node->children[word[i]-'a']==nullptr)
+            {
+                return false;
+            }
+            cur_node=cur_node->children[word[i++]-'a'];
+        }
+        if(cur_node->isWord)
+        return true;
+        return false;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Node * cur_node=root;
+        int i=0;
+        while(i<prefix.size()){
+            if(cur_node->children[prefix[i]-'a']==nullptr)
+            {
+                return false;
+            }
+            cur_node=cur_node->children[prefix[i++]-'a'];
+        }
+        return true;
+        
+    }
+};
+
